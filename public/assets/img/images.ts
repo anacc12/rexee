df31f3fdb8c756898293bc52f2afa8e82b0d50a9
anacc12 <cacijaana2@gmail.com>
2024-12-02 15:58:34 +0100

    Added assets to public

---
+++ public/assets/img/images.ts
@@ -0,0 +1,60 @@
+// IMAGES
+import SelectCountry from "./select-country.png";
+import VoiceQuestion from "./voice-question.png";
+import Reward from "./reward.png";
+import Login from "./login.png";
+import Voucher from "./voucher.png";
+import LoginInputs from "./login-inputs.png";
+import Dashboard from "./dashboard.png";
+import Leaderboard from "./leaderboard.png";
+import ItemUnlocked from "./item-unlocked.png";
+
+// TEAM
+import Mario from "./team/mario.webp";
+import Anna from "./team/anna.webp";
+import Riccardo from "./team/riccardo.webp";
+import Ennio from "./team/ennio.webp";
+import Alessandro from "./team/alessandro.webp";
+import Emily from "./team/emily.webp";
+import Marko from "./team/marko.webp";
+import Manuel from "./team/manuel.webp";
+
+// TEAM HOVER
+import MarioHover from "./team/mario-hover.webp";
+import AnnaHover from "./team/anna-hover.webp";
+import RiccardoHover from "./team/riccardo-hover.webp";
+import EnnioHover from "./team/ennio-hover.webp";
+import AlessandroHover from "./team/alessandro-hover.webp";
+import EmilyHover from "./team/emily-hover.webp";
+import MarkoHover from "./team/marko-hover.webp";
+import ManuelHover from "./team/manuel-hover.webp";
+
+const Image = {
+  SelectCountry,
+  VoiceQuestion,
+  Reward,
+  Login,
+  Voucher,
+  LoginInputs,
+  Dashboard,
+  Leaderboard,
+  ItemUnlocked,
+  Mario,
+  Anna,
+  Riccardo,
+  Ennio,
+  Alessandro,
+  Emily,
+  Marko,
+  Manuel,
+  MarioHover,
+  AnnaHover,
+  RiccardoHover,
+  EnnioHover,
+  AlessandroHover,
+  EmilyHover,
+  MarkoHover,
+  ManuelHover,
+};
+
+export default Image;
---
+++ public/assets/img/items.ts
@@ -0,0 +1,27 @@
+// TEAM
+import Item1 from "./items/1.png";
+import Item2 from "./items/2.png";
+import Item3 from "./items/3.png";
+import Item5 from "./items/5.png";
+import Item6 from "./items/6.png";
+import Item7 from "./items/7.png";
+import Item8 from "./items/8.png";
+import Item9 from "./items/9.png";
+import Item10 from "./items/10.png";
+import Item11 from "./items/11.png";
+import Item12 from "./items/12.png";
+import Item14 from "./items/14.png";
+import Item15 from "./items/15.png";
+import Item16 from "./items/16.png";
+import Item17 from "./items/17.png";
+import Item18 from "./items/18.png";
+import Item19 from "./items/19.png";
+import Unknown from "./items/unknown.png";
+
+
+
+const Item = {
+    Item1, Item2, Item3, Item5, Item6, Item7, Item8, Item9, Item10, Item11, Item12, Item14, Item15, Item16, Item17, Item18, Item19, Unknown
+};
+
+export default Item;
